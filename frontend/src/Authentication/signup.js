d812bcefb1942e60a7263a8fc6fc7983617868ea
yaoyuan <yaoyuan@u.nus.edu>
2021-10-14 20:49:51 +0800

    basic login signup

---
+++ frontend/src/Authentication/signup.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export class Signup extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="header"> Sign up </div>
+        <div className="content">
+          <div className="form">
+            <div className="sub-form">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="email" />
+            </div>
+            <div className="sub-form">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="password" />
+            </div>
+            <div className="sub-form">
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input
+                type="password"
+                name="confirm-password"
+                placeholder="confirm-password"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
