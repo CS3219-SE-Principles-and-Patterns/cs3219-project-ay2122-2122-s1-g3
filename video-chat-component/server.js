2d7429561befeee57a86b840ae971817a9232573
GeNiaaz <geniaaz1009@gmail.com>
2021-10-16 11:56:30 +0800

    add video chat component, works locally

---
+++ video-chat-component/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { SocketAddress } = require("net");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    console.log("room id:", roomId, "user id:", userId);
+    // tell others user has joined
+    socket.join(roomId);
+    // send message to current room
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+
+server.listen(3000);
