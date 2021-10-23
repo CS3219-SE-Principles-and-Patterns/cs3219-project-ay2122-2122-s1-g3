1561880706e7e5ed0a992c84f3a615a3933c097f
iamjamestan <jamestanyuli@gmail.com>
2021-10-23 23:26:42 +0800

    Use input for buttons and fix alignment in video and question

--- frontend/src/Room/questionSection.js
+++ frontend/src/Room/questionSection.js
@@ -1,27 +1,29 @@
-import "./questionSectionStyle.scss"
-import {Video} from "./video"
-import {Button} from "react-bootstrap"
+import "./questionSectionStyle.scss";
+import { Video } from "./video";
 export const QuestionSection = () => {
-    return (
-        <div className="QuestionSection">
-            <div className="questionPortion">
-                <div className="questionHeader">
-                    <div className="nextButtonWrapper">
-                        <Button className="nextButton" variant="outline-dark">
-                            Next Question
-                        </Button>
-                    </div>
-                    <h3 className="questionTitle">
-                        Single Threaded CPU
-                    </h3>
-                </div>
-                <div className="questionBody">
-                    <h6 className="questionDescription">
-                    You are given n​​​​​​ tasks labeled from 0 to n - 1 represented by a 2D integer array tasks, where tasks[i] = [enqueueTimei, processingTimei] means that the i​​​​​​th​​​​ task will be available to process at enqueueTimei and will take processingTimei to finish processing.
-                    </h6>
-                </div>
-            </div>
-            <Video />
+  return (
+    <div className="QuestionSection">
+      <div className="questionPortion">
+        <div className="questionHeader">
+          <div className="nextButton">
+            <input
+              type="button"
+              value="Next"
+            />
+          </div>
+          <h3 className="questionTitle">Single Threaded CPU</h3>
         </div>
-    )
-}
+        <div className="questionBody">
+          <h6 className="questionDescription">
+            You are given n​​​​​​ tasks labeled from 0 to n - 1 represented by a
+            2D integer array tasks, where tasks[i] = [enqueueTimei,
+            processingTimei] means that the i​​​​​​th​​​​ task will be available
+            to process at enqueueTimei and will take processingTimei to finish
+            processing.
+          </h6>
+        </div>
+      </div>
+      <Video />
+    </div>
+  );
+};
--- frontend/src/Room/video.js
+++ frontend/src/Room/video.js
@@ -1,23 +1,30 @@
-import "./videoStyle.scss"
+import "./videoStyle.scss";
 export const Video = () => {
-    return (
-        <div className="Video">
-            <div className="leftButtons">
-                <button type="button" className="btn">
-                    Video
-                </button>
-                <button type="button" className="btn">
-                    Microphone
-                </button>
-            </div>
-            <div className="cameraFeed">
-                Rollrollfaraway video
-            </div>
-            <div className="rightButtons">
-                <button type="button" className="btn">
-                    Smiley
-                </button>
-            </div>
+  return (
+    <div className="Video">
+      <div className="leftButtons">
+        <div className="videoButton">
+          <input
+            type="button"
+            value="Video"
+          />
         </div>
-    )
-}
+        <div className="microphoneButton">
+          <input
+            type="button"
+            value="Microphone"
+          />
+        </div>
+      </div>
+      <div className="cameraFeed">Rollrollfaraway video</div>
+      <div className="rightButtons">
+        <div className="smileyButton">
+          <input
+            type="button"
+            value="Smiley"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
