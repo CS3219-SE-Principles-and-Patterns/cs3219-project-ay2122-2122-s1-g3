889e40a2ff8802cd7a11a765345ddd5b40be5f01
iamjamestan <jamestanyuli@gmail.com>
2021-10-24 15:29:10 +0800

    Fix style for code section

--- frontend/src/Room/codeSection.js
+++ frontend/src/Room/codeSection.js
@@ -1,27 +1,19 @@
-import "./codeSectionStyle.scss"
-import {Editor} from "./editor"
-import {Terminal} from "./terminal"
-//import {Button} from "react-bootstrap"
+import "./codeSectionStyle.scss";
+import { Editor } from "./editor";
+import { Terminal } from "./terminal";
 
 export const CodeSection = () => {
-    return (
-        <div className="CodeSection">
-            <div className="codeHeader">
-                <div className="nextButtonWrapper">
-                        {/* <Button className="nextButton" variant="danger">
-                            Exit
-                        </Button> */}
-                </div>
-                <h3 className="questionTitle">
-                    Your Code
-                </h3>
-            </div>
-
-            <Editor />
-            <h5 className="terminalHeader">
-                Standard Output
-            </h5>
-            <Terminal />
+  return (
+    <div className="CodeSection">
+      <div className="codeHeader">
+        <div className="nextButtonWrapper">
+          <button>Exit</button>
         </div>
-    )
-}
+        <h3 className="questionTitle">Your Code</h3>
+      </div>
+      <Editor />
+      <h5 className="terminalHeader">Standard Output</h5>
+      <Terminal />
+    </div>
+  );
+};
