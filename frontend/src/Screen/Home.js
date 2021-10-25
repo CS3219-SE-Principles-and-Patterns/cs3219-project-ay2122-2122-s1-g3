1e8c578fbc7b6804096589d1031be55cdaef54cb
iamjamestan <jamestanyuli@gmail.com>
2021-10-25 14:55:33 +0800

    Style home page

--- frontend/src/Screen/Home.js
+++ frontend/src/Screen/Home.js
@@ -1,23 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { getUser, removeUserSession } from "../Utils/Common";
 
+import "../Style/homeStyle.scss";
+
 function Home(props) {
+  const difficulty = useFormInput("Easy");
+  const category = useFormInput("Array");
+  const language = useFormInput("Python");
   const user = getUser();
 
-  // handle click event of logout button
-  const handleLogout = () => {
-    removeUserSession();
-    props.history.push("/login");
+  // handle submission of form
+  const handleSubmit = (e) => {
+    console.log(difficulty.value);
+    console.log(category.value);
+    console.log(language.value);
+    props.history.push("/room");
+    e.preventDefault();
   };
 
   return (
-    <div>
-      This is homepage
-      <br />
-      <br />
-      <input type="button" onClick={handleLogout} value="Logout" />
+    <div className="Home">
+      <div className="homeHeader">
+        <div className="brandName">PeerPrep</div>
+      </div>
+      <div className="homeBody">
+        <form onSubmit={handleSubmit}>
+          <label>Difficulty</label>
+          <select {...difficulty}>
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Difficult">Difficult</option>
+          </select>
+          <label>Category</label>
+          <select {...category}>
+            <option value="Array">Array</option>
+            <option value="Binary">Binary</option>
+            <option value="Dynamic Programming">Dynamic Programming</option>
+            <option value="Graph">Graph</option>
+            <option value="Heap">Heap</option>
+            <option value="Interval">Interval</option>
+            <option value="Linked List">Linked List</option>
+            <option value="Matrix">Matrix</option>
+            <option value="String">String</option>
+            <option value="Tree">Tree</option>
+            <option value="Recursion">Recursion</option>
+          </select>
+          <label>Language</label>
+          <select {...language}>
+            <option value="Python">Python</option>
+            <option value="Javascript">Javascript</option>
+          </select>
+          <button type="submit" value="Submit">
+            Let's go
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
+const useFormInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  return {
+    value,
+    onChange: handleChange,
+  };
+};
 
 export default Home;
