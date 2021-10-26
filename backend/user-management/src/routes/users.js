4d861d5b5b1b7b4fe64a93b393473f30767b4aa2
Galvin <galvinleow@gmail.com>
2021-10-26 15:44:32 +0800

    Update Password and Get User Info

--- backend/user-management/src/routes/users.js
+++ backend/user-management/src/routes/users.js
@@ -1,10 +1,42 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../model/user-model");
+const { handleResponse } = require("../utils/utils");
 
 // request handlers
 router.get("/", (req, res) => {
   res.send("Welcome to CS3219 Group Work - Users");
+});
+
+router.put("/updatePassword/:id", async function (req, res) {
+  if (!req.body.hasOwnProperty("password")) {
+    return handleResponse(req, res, 400, null, "New password required.");
+  }
+  const hash = await bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      return hash;
+    })
+    .catch((err) => console.error(err));
+  req.body.password = hash;
+
+  User.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return handleResponse(req, res, 400, null, err);
+    if (!post) return handleResponse(req, res, 400, null, err);
+    User.findById(req.params.id, function (err, post) {
+      if (err) return handleResponse(req, res, 400, null, err);
+      res.json(post);
+    });
+  });
+});
+
+router.get("/getSingleUser/:id", function (req, res) {
+  User.findById(req.params.id, function (err, post) {
+    if (err) return handleResponse(req, res, 400, null, err);
+
+    res.json(post);
+  });
 });
 
 module.exports = router;
